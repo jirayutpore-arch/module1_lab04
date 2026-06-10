a30d538aa2981f35a1f9334ef0e55e42c7e7fd12
jirayutpore-arch <jirayut.pore@gmail.com>
2026-06-10 15:44:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,8 @@ pages = [
     {"icon": "📈", "label": "พยากรณ์ยอดขาย", "path": "pages/sale_predict.py", "desc": "แอปลิเคชันนี้ช่วยพยากรณ์ยอดขาย"},
     {"icon": "🚀", "label": "Red Bull High Sales Predictor", "path": "pages/classify_redbull_sale.py", "desc": "เครื่องมือนี้ช่วยคาดการณ์ว่า Product จะมียอดขายสูงหรือไม่ ขึ้นอยู่กับงบประมาณการตลาดและปัจจัยอื่นๆ"},
     {"icon": "📈", "label": "Market Segmentation Predictor", "path": "pages/clustering_segment.py", "desc": "การจัดกลุ่มข้อมูลจากข้อมูลลูกค้า."},
-    {"icon": "🛒", "label": "TCP Group: ระบบแนะนำสินค้าจาก Market Basket Analysis", "path": "pages/association_items.py", "desc": "แนะนำสินค้าที่น่าจะซื้อคู่กัน"}
+    {"icon": "🛒", "label": "TCP Group: ระบบแนะนำสินค้าจาก Market Basket Analysis", "path": "pages/association_items.py", "desc": "แนะนำสินค้าที่น่าจะซื้อคู่กัน"},
+    {"icon": "🗺", "label": "TCP Behavioral Association Recommendation", "path": "pages/association_items.py", "desc": "ให้ระบบแนะนำสินค้า/ช่องทาง/ภูมิภาคอื่น ๆ ที่ลูกค้ามีแนวโน้มจะสนใจ จากกฎความสัมพันธ์ "}
 ]
 
 total_slides = len(pages)
